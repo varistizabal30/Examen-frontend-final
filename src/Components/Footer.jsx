ad9498c475054926058ea3a3e7f1bce8f7c27af6
MarcelaMesaLeon <mardie05@hotmail.com>
2023-04-04 20:29:43 -0500

    Remove width app.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,12 @@
+import style from '../Styles/footer.module.css'
 
 const Footer = () => {
   const scrollToTop = () => {
     window.scrollTo(0, 0)
   }
   return (
-    <footer>
-        <h2>Footer</h2>
+    <footer >
+        <h2 className={style.fo}>Footer</h2>
     </footer >
   )
 }
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -7,7 +7,7 @@ import { AppContext } from "../Context/AppContext";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
-  const { theme, toggleTheme } = useContext(AppContext);
+  const { theme } = useContext(AppContext);
   return (
     <div className={theme === "dark" ? style.dark : style.ligth}>
       <h2>Want to know more?</h2>
